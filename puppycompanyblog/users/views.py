c941c534b224bed29bcbdfdf5493dffb72d84f2d
Jorge Solis <solisjorge1302@gmail.com>
2021-11-02 16:22:43 -0600

    Completing proyect

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -75,8 +75,8 @@ def account():
     return render_template('account.html',profile_image=profile_image, form= form)
 
 @users.route("/<username>")
-def user_post(username):
+def user_posts(username):
     page = request.args.get('page',1,type=int)
     user = User.query.filter_by(username=username).first_or_404()
-    blog_post = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page,per_page = 5)
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page,per_page = 5)
     return render_template('user_blog_posts.html',blog_posts = blog_posts, user=user)
